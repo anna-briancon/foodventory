69e76c7d2ae11ed5f753b96042ac9273727619df
Anna <anna.briancon33@gmail.com>
2024-10-06 19:49:02 +0200

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // Remove the output: 'export' line if you want to use API routes and server-side features
+//   // output: 'export',
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Remove the output: 'export' line if you want to use API routes and server-side features
-    // output: 'export',
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  export default nextConfig;
+  output: 'export',
+  basePath: '/foodventory',
+  assetPrefix: '/foodventory/',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;

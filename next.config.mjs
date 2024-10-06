d6eedfdbab78f67ba49518a0aeedf33825db4f3c
Anna <anna.briancon33@gmail.com>
2024-10-06 12:11:16 +0200

    Configuration initiale

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: '/foodventory.github.io',
+  };
+  
+  export default nextConfig;
